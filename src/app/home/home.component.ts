c95aca0b582fdcc8783d561669326182db3e04cf
Ana Raquel Campesan <ana@mediahub.com>
2017-09-11 11:45:16 -0400

    Refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { PokemonService } from '../_services/pokemon.service';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/concat';
-import 'rxjs/add/observable/from';
 
 @Component({
   selector: 'app-home',
@@ -16,22 +14,19 @@ import 'rxjs/add/observable/from';
 export class HomeComponent implements OnInit {
   pagination = 0;
 
-  pokemonStats: Observable<any>[]=[];
+  pokemonStats: Observable<any>[] = [];
   pokemon$ = new Subject<any>();
 
   constructor(private _pokemon: PokemonService,
-              private _http: HttpClient) {
-    this._pokemon.getByName(this.pokemon$)
-        .subscribe(results => {
-          this.pokemonStats.push(results);
-        });
-  }
+              private _http: HttpClient) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this._pokemon.getByName(this.pokemon$).subscribe(pokemon => {
+      this.pokemonStats.push(pokemon);
+    });
+  }
 
   print() {
     console.log(this.pokemonStats);
   }
-
-
 }
